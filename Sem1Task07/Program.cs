abab5c349e5ca90af867d02598a35c10263caf70
Arad Arefi <a.arefi@mail.ru>
2023-03-24 00:12:48 +0300

    add 4 tasks Sem1

---
+++ Sem1Task07/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Put your number: ");
+string? num = Console.ReadLine();
+
+if (num!=null)
+{
+   int intNumber = int.Parse(num);
+   int res = intNumber % 10;
+   Console.Write(res);
+
+}
+
+
