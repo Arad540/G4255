92eb6e57be0622cdc0f56365f4b7446c22a93821
Арефи Арад <a.arefi@internal.avilex.ru>
2023-03-28 15:37:19 +0300

    update task Sem2Task13

--- Sem2Task13/Program.cs
+++ Sem2Task13/Program.cs
@@ -1,15 +1,22 @@
 ﻿int num = int.Parse(Console.ReadLine()??"0");
 
-int Numthird = num%1000;
-int NumCheck = num/1000;
+int res = num;
 
-if (NumCheck > 0)
-{
-    int secondNum = Numthird / 100;
-    Console.WriteLine("secondNum);
+if (num <100 )
 
-}
+Console.Write("The 3rd number does not exist");
+
 else
-{  
-    Console.WriteLine(" number contanis less that 3 digits ");
+{
+    
+
+while (res >= 1000)
+{
+    res = res /10;
+ 
 }
+
+res = res%10;
+}
+Console.Write( res );
+
