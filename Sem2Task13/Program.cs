d7df2781f3699ef286d07971640bfaa4e3d8af69
Арефи Арад <a.arefi@internal.avilex.ru>
2023-03-28 12:56:22 +0300

    add task Sem2Task13

---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,15 @@
+﻿int num = int.Parse(Console.ReadLine()??"0");
+
+int Numthird = num%1000;
+int NumCheck = num/1000;
+
+if (NumCheck > 0)
+{
+    int secondNum = Numthird / 100;
+    Console.WriteLine("secondNum);
+
+}
+else
+{  
+    Console.WriteLine(" number contanis less that 3 digits ");
+}
