355640f01b2c8c208d8febe98c40c18570aa4f88
Арефи Арад <a.arefi@internal.avilex.ru>
2023-03-29 13:15:18 +0300

    add Sem2Task15

---
+++ Sem2Task15/Program.cs
@@ -0,0 +1,18 @@
+﻿int number = int.Parse(Console.ReadLine()??"0");
+
+if (number >= 1 && number <  6 )
+
+   Console.Write(number + " is not weekEnd");
+
+else if (number>7)
+         
+         Console.Write(number + "   The number cant be day of weekend");
+     else
+     
+         Console.Write(number + " is weekEnd");
+
+
+
+    
+
+
