827e7da1d81e342f946bc4c7a5d30f7a2e319c78
Arad Arefi <a.arefi@mail.ru>
2023-04-05 23:16:33 +0300

    add final  tasks 27

--- Sem4Task27/Program.cs
+++ Sem4Task27/Program.cs
@@ -1,14 +1,20 @@
 ﻿
+Console.Clear();
+Console.Write("Please put your number: ");
 
 string? num = Console.ReadLine();
 char[] arr = num.ToCharArray();
-//Console.WriteLine(arr.Length);
 int i = 0;
 int res = 0;
 while ( i < arr.Length)
 {
 res = res + arr[i]-48 ;
 i++;
-}Console.WriteLine("sum of length " +res);
+}
+Console.WriteLine("");
+Console.WriteLine("Length of arrey: " + arr.Length);
+Console.WriteLine("");
+Console.WriteLine("Sum of length: " +res);
+Console.WriteLine("");
 
 
