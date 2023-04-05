a89535203eb3eaad172f857aa380c6d9b85570c9
Arad Arefi <a.arefi@mail.ru>
2023-04-05 22:41:32 +0300

    add tasks 27

---
+++ Sem4Task27/Program.cs
@@ -0,0 +1,31 @@
+﻿// int ReadNum1(string message)
+// {
+//     Console.WriteLine(message);
+//     int num1 = int.Parse(Console.ReadLine() ?? "0");
+//     return num1;
+// }
+
+// string intToChar (string arr[], int num)
+// {
+// char [] arr1 = num.toChararrey();
+// return arr1;
+// }
+
+string? num = Console.ReadLine();
+
+char[] arr = num.ToCharArray();
+Console.WriteLine(arr.Length);
+
+int i = 0;
+int res = 0;
+
+while ( i < arr.Length)
+{
+
+res = res + arr[i] ;
+i++;
+Console.WriteLine(res);
+
+}
+
+
